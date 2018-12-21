bc7ad61d391f2158d01436a9d23ea0db2b8ff6ad
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-12-20 17:00:00 -0700

    examples: temperature doesnt loop

--- examples/temperature/main.rs
+++ examples/temperature/main.rs
@@ -1,5 +1,4 @@
 use smc::{Kind, Smc, SmcResult};
-use std::{thread, time};
 
 fn main() -> SmcResult<()> {
     let smc = Smc::new()?;
@@ -7,10 +6,9 @@ fn main() -> SmcResult<()> {
     // sensors have nontrivial newup cost so you may wish to keep these around
     let sensors = smc.find(|key| key.kind() == Kind::Temperature);
 
-    loop {
-        for sensor in sensors.clone() {
-            println!("{:?}, {}", sensor.name(), sensor.read()?);
-        }
-        thread::sleep(time::Duration::from_millis(10000));
+    for sensor in sensors.clone() {
+        println!("{}", sensor);
     }
+
+    Ok(())
 }
