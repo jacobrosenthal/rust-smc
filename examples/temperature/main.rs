22bbc6d98647da426987e612774e27d428d1dab7
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-12-23 10:53:36 -0700

    examples: temperature update every second

--- examples/temperature/main.rs
+++ examples/temperature/main.rs
@@ -1,4 +1,5 @@
 use smc::{Kind, Smc, SmcResult};
+use std::{thread, time};
 
 fn main() -> SmcResult<()> {
     let smc = Smc::new()?;
@@ -6,9 +7,17 @@ fn main() -> SmcResult<()> {
     // sensors have nontrivial newup cost so you may wish to keep these around
     let sensors = smc.find(|key| key.kind() == Kind::Temperature);
 
-    for sensor in sensors.clone() {
-        println!("{}", sensor);
-    }
+    let count = sensors.clone().count();
+    loop {
+        for sensor in sensors.clone() {
+            println!("{}", sensor);
+        }
 
-    Ok(())
+        thread::sleep(time::Duration::from_millis(1000));
+
+        for _x in 0..count {
+            //back up and clear the line so we write over the top of it
+            print!("\x1b[1A\x1b[K");
+        }
+    }
 }
