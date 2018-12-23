1855b36de1916cd54bbc478f65ad33d8154aa637
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-12-23 14:20:20 -0700

    examples: simplify temperature and add all

--- examples/temperature/main.rs
+++ examples/temperature/main.rs
@@ -1,5 +1,4 @@
 use smc::{Kind, Smc, SmcResult};
-use std::{thread, time};
 
 fn main() -> SmcResult<()> {
     let smc = Smc::new()?;
@@ -7,17 +6,9 @@ fn main() -> SmcResult<()> {
     // sensors have nontrivial newup cost so you may wish to keep these around
     let sensors = smc.find(|key| key.kind() == Kind::Temperature);
 
-    let count = sensors.clone().count();
-    loop {
-        for sensor in sensors.clone() {
-            println!("{}", sensor);
-        }
-
-        thread::sleep(time::Duration::from_millis(1000));
-
-        for _x in 0..count {
-            //back up and clear the line so we write over the top of it
-            print!("\x1b[1A\x1b[K");
-        }
+    for sensor in sensors.clone() {
+        println!("{}", sensor);
     }
+
+    Ok(())
 }
